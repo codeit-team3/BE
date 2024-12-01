dfe6c5709992aea972d96640d4cbc83049066488
coen <bht9011@gmail.com>
2024-12-01 22:03:25 +0900

    feat: 북클럽 리뷰 작성 API

---
+++ src/main/java/com/codeit/sprint/team3/backend/bookclub/application/service/BookClubReviewService.java
@@ -0,0 +1,23 @@
+package com.codeit.sprint.team3.backend.bookclub.application.service;
+
+import com.codeit.sprint.team3.backend.bookclub.application.port.in.BookClubUseCase;
+import com.codeit.sprint.team3.backend.bookclub.application.port.out.BookClubReviewPort;
+import com.codeit.sprint.team3.backend.bookclub.application.port.in.BookClubReviewUseCase;
+import com.codeit.sprint.team3.backend.bookclub.domain.BookClub;
+import com.codeit.sprint.team3.backend.bookclub.domain.BookClubReview;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookClubReviewService implements BookClubReviewUseCase {
+    private final BookClubUseCase bookClubUseCase;
+    private final BookClubReviewPort bookClubReviewPort;
+
+    @Override
+    public void saveBookClubReview(Long bookClubId, Long userId, Integer rating, String content) {
+        BookClub bookClub = bookClubUseCase.getById(bookClubId);
+        BookClubReview bookClubReview = BookClubReview.of(bookClub.getId(), userId, rating, content);
+        bookClubReviewPort.saveBookClubReview(bookClubReview);
+    }
+}
