d97269ab2d158fe85c70659849fd9667fd8d3970
coen <bht9011@gmail.com>
2024-12-09 20:20:23 +0900

    fix: 리뷰 내용 에러메시지 추가

--- src/main/java/com/codeit/sprint/team3/backend/bookclub/adapter/in/web/request/CreateBookClubReviewRequest.java
+++ src/main/java/com/codeit/sprint/team3/backend/bookclub/adapter/in/web/request/CreateBookClubReviewRequest.java
@@ -10,7 +10,7 @@ public record CreateBookClubReviewRequest(
         @Min(value = 1, message = "별점은 최소 1점 입니다.")
         @NotNull(message = "별점은 필수입니다.")
         Integer rating,
-        @NotBlank
+        @NotBlank(message = "리뷰 내용은 필수입니다.")
         String content
 ) {
 }
