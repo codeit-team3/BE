491434b63b1a4f60a2e7dff8a5d7130cc8ea78f1
coen <bht9011@gmail.com>
2024-12-09 20:12:13 +0900

    fix: 사용하지 않는 import 제거

--- src/main/java/com/codeit/sprint/team3/backend/bookclub/application/port/in/BookClubLikeUseCase.java
+++ src/main/java/com/codeit/sprint/team3/backend/bookclub/application/port/in/BookClubLikeUseCase.java
@@ -1,7 +1,5 @@
 package com.codeit.sprint.team3.backend.bookclub.application.port.in;
 
-import com.codeit.sprint.team3.backend.bookclub.domain.BookClub;
-
 public interface BookClubLikeUseCase {
     void saveBookClubLike(Long bookClubId, Long userId);
 
