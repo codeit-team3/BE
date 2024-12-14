56b967b120ad628930ba7d3eb205d1b422c7c709
coen <bht9011@gmail.com>
2024-12-14 14:21:04 +0900

    feature: 북클럽 리뷰 점수 조정 (최소1 최대5)

--- src/main/java/com/codeit/sprint/team3/backend/bookclub/adapter/in/web/request/CreateBookClubReviewRequest.java
+++ src/main/java/com/codeit/sprint/team3/backend/bookclub/adapter/in/web/request/CreateBookClubReviewRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateBookClubReviewRequest(
-        @Max(value = 10, message = "별점은 최대 10점 입니다.")
+        @Max(value = 5, message = "별점은 최대 5점 입니다.")
         @Min(value = 1, message = "별점은 최소 1점 입니다.")
         @NotNull(message = "별점은 필수입니다.")
         Integer rating,
