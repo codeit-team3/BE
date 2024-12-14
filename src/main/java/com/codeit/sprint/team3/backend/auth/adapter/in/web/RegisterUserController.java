790d8ed587a4524270a4859c27a500b5619fe989
Junha <yy4124@gmail.com>
2024-12-14 18:01:21 +0900

    Swagger 작업 및 로그아웃 인증 접근 해제 (#10)

--- src/main/java/com/codeit/sprint/team3/backend/auth/adapter/in/web/RegisterUserController.java
+++ src/main/java/com/codeit/sprint/team3/backend/auth/adapter/in/web/RegisterUserController.java
@@ -2,12 +2,15 @@ package com.codeit.sprint.team3.backend.auth.adapter.in.web;
 
 import com.codeit.sprint.team3.backend.auth.application.port.in.RegisterUserRequest;
 import com.codeit.sprint.team3.backend.auth.application.port.in.RegisterUserUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Auths")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auths")
@@ -16,6 +19,7 @@ public class RegisterUserController {
     private final RegisterUserUseCase registerUserUseCase;
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
     public ResponseEntity<Void> register(
             @RequestBody @Valid RegisterUserRequest request
     ) {
