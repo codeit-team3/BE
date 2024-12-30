a0ee5dbd9ba99ac4e28c7e3ba290359e83c92a1d
coen <bht9011@gmail.com>
2024-12-30 21:35:11 +0900

    fix: 오류 수정

--- src/main/java/com/codeit/sprint/team3/backend/bookclub/adapter/in/web/BookClubController.java
+++ src/main/java/com/codeit/sprint/team3/backend/bookclub/adapter/in/web/BookClubController.java
@@ -51,7 +51,7 @@ public class BookClubController {
     }
 
     private void validateImage(MultipartFile image) {
-        if (VALID_EXTENSIONS.contains(StringUtils.getFilenameExtension(image.getOriginalFilename()))) {
+        if (!VALID_EXTENSIONS.contains(StringUtils.getFilenameExtension(image.getOriginalFilename()))) {
             throw new InvalidRequest("image", String.format("이미지는 %s 형식이어야 합니다.", String.join(", ", VALID_EXTENSIONS)));
         }
         long size = image.getSize();
