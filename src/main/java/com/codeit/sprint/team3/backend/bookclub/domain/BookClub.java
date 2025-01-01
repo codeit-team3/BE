9c0319ab7d9504a798201f685146eec281153e46
coen <bht9011@gmail.com>
2025-01-02 01:06:02 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/com/codeit/sprint/team3/backend/bookclub/domain/BookClub.java
+++ src/main/java/com/codeit/sprint/team3/backend/bookclub/domain/BookClub.java
@@ -58,21 +58,6 @@ public class BookClub {
                 .build();
     }
 
-    public static BookClub of(Long id, String title, String description, MeetingType meetingType, BookClubType bookClubType, LocalDateTime targetDate, LocalDateTime endDate, int memberLimit, String town, Long createdBy) {
-        return BookClub.builder()
-                .id(id)
-                .title(title)
-                .description(description)
-                .meetingType(meetingType)
-                .bookClubType(bookClubType)
-                .targetDate(targetDate)
-                .endDate(endDate)
-                .memberLimit(memberLimit)
-                .town(town)
-                .createdBy(createdBy)
-                .build();
-    }
-
     public static BookClub of(Long id, String title, String description, MeetingType meetingType, BookClubType bookClubType, LocalDateTime targetDate, LocalDateTime endDate, int memberLimit, String town, Long createdBy, boolean isInactive) {
         return BookClub.builder()
                 .id(id)
